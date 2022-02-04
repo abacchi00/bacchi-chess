264ef3ca95e18bbc22cf3d6c90d129b31713b755
André Bacchi <andre.rossini.bacchi@gmail.com>
2022-02-04 00:20:16 -0300

    Create export file to all pieces imgs

---
+++ src/assets/piece_imgs/index.ts
@@ -0,0 +1,27 @@
+import pawn_dark_img from './pawn_dark.svg';
+import pawn_light_img from './pawn_light.svg';
+import bishop_dark_img from './bishop_dark.svg';
+import bishop_light_img from './bishop_light.svg';
+import rook_dark_img from './rook_dark.svg';
+import rook_light_img from './rook_light.svg';
+import knight_dark_img from './knight_dark.svg';
+import knight_light_img from './knight_light.svg';
+import king_dark_img from './king_dark.svg';
+import king_light_img from './king_light.svg';
+import queen_dark_img from './queen_dark.svg';
+import queen_light_img from './queen_light.svg';
+
+export const pieceImgs = {
+  pawn_dark: pawn_dark_img,
+  pawn_light: pawn_light_img,
+  bishop_dark: bishop_dark_img,
+  bishop_light: bishop_light_img,
+  rook_dark: rook_dark_img,
+  rook_light: rook_light_img,
+  knight_dark: knight_dark_img,
+  knight_light: knight_light_img,
+  king_dark: king_dark_img,
+  king_light: king_light_img,
+  queen_dark: queen_dark_img,
+  queen_light: queen_light_img,
+}
